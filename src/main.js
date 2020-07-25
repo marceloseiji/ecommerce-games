f194d9580e76cd87e332dcfccc944d14ef1d253a
Marcelo Seiji <ikuta.marcelo@gmail.com>
2020-07-25 20:19:13 -0300

    components and methods imported/exported

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Home from "./components/Home";
+import VueRouter from "vue-router";
+import request from "./request";
+
+Vue.prototype.$request = request;
 
 Vue.config.productionTip = false;
 
+//Cria os objetos para as rotas
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+    component: Home,
+  }
+];
+
+//Instacia o router do vue
+const router = new VueRouter({ mode: "history", routes });
+
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App),
+  router, //Passa para o vue as configurações das rotas
 }).$mount("#app");
