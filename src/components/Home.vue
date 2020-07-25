57c5e0cc599924c427f3dc8d7a683c5186df5c96
Marcelo Seiji <ikuta.marcelo@gmail.com>
2020-07-25 20:17:07 -0300

    Home component added

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>Homes</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+  },
+  data(){
+    return {
+
+    }
+  },
+  methods: {
+    showAll() {
+      console.log("This ");
+    }
+  },
+  mounted() {
+    this.showAll();
+  }
+};
+</script>
+
+<style lang="scss">
+  .hello {
+    h1 {
+      color: red;
+    }
+  }
+</style>
