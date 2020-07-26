b2a1f9e2aa8c9743782b4edf40ffabd4c9a8b08b
Marcelo Seiji <ikuta.marcelo@gmail.com>
2020-07-25 22:07:20 -0300

    GameCard removed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,19 +2,29 @@
   <div class="hello">
     <h1>Home</h1>
     <div class="col-12 col-md-4" v-for="item in items" v-bind:key="item.id">
-      <GameCard />
+      <b-card
+        title="Card Title"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </b-card-text>
+
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
     </div>
   </div>
 </template>
 
 <script>
-import GameCard from "./GameCard.vue";
-
 export default {
   name: "Home",
-  components: {
-    GameCard,
-  },
   props: {},
   data() {
     return {
