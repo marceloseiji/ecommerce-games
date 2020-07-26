6ecfd1cd6ab4866b5d09e911b224923b68278f2d
Marcelo Seiji <ikuta.marcelo@gmail.com>
2020-07-25 22:57:39 -0300

    card style & structure, data

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="hello">
-    <h1>Home</h1>
-    <div class="col-12 col-md-4" v-for="item in items" v-bind:key="item.id">
-      <b-card
-        title="Card Title"
-        img-src="https://picsum.photos/600/300/?image=25"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 20rem;"
-        class="mb-2"
+  <div class="container home">
+    <h1 class="title">Games</h1>
+    <div class="row">
+      <div
+        class="col-sm-12 col-md-6 col-lg-4"
+        v-for="item in items"
+        v-bind:key="item.id"
       >
-        <b-card-text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </b-card-text>
-
-        <b-button href="#" variant="primary">Go somewhere</b-button>
-      </b-card>
+        <b-card
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+        >
+          <div class="img-holder">
+            <img :src="require(`@/assets/${item.image}`)" />
+          </div>
+          <h4 class="card-title">{{ item.name }}</h4>
+          <b-card-text>
+            {{ item.price }}
+          </b-card-text>
+        </b-card>
+      </div>
     </div>
   </div>
 </template>
@@ -46,9 +51,30 @@ export default {
 </script>
 
 <style lang="scss">
-.hello {
-  h1 {
-    color: red;
+@import "../variables.scss";
+.home {
+  font-family: $font;
+  .title {
+    text-align: left;
+  }
+  .card {
+    .img-holder {
+      display: flex;
+      height: 230px;
+      background-color: $color1;
+      img {
+        width: 180px;
+        height: 180px;
+        margin: auto;
+      }
+    }
+    .card-body {
+      padding: 0;
+      .card-title {
+      }
+      .card-text {
+      }
+    }
   }
 }
 </style>
