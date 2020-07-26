b1d58dfb4ab38d2b6648c7a295ac5bf22561bbd3
Marcelo Seiji <ikuta.marcelo@gmail.com>
2020-07-26 20:04:48 -0300

    vue router removed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Home from "./components/Home";
-import VueRouter from "vue-router";
 
 //Controllers request, cart methods
 import controllers from "./controllers";
@@ -18,19 +16,6 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
-//Cria os objetos para as rotas
-const routes = [
-  {
-    name: "Home",
-    path: "/",
-    component: Home,
-  },
-];
-
-//Instacia o router do vue
-const router = new VueRouter({ mode: "history", routes });
-
 new Vue({
   render: (h) => h(App),
-  router, //Passa para o vue as configurações das rotas
 }).$mount("#app");
