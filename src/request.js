d59b88a60c87189462bee4d307cc033181a412c2
Marcelo Seiji <ikuta.marcelo@gmail.com>
2020-07-25 23:01:26 -0300

    file cleaned

--- src/request.js
+++ src/request.js
@@ -2,7 +2,7 @@ export default class request {
   static async getAll() {
     const request = new Request(
       "https://raw.githubusercontent.com/ConsultaRemedios/frontend-challenge/master/products.json",
-      { 
+      {
         method: "GET",
       }
     );
@@ -10,26 +10,6 @@ export default class request {
     const dataObj = await res.json();
 
     Object.assign({}, dataObj);
-
-    // dataObj.forEach(function(element) {
-    //   dataObj[element.id] = element;
-    // });
     return dataObj;
   }
 }
-
-// const url = "https://raw.githubusercontent.com/ConsultaRemedios/frontend-challenge/master/products.json";
-// return fetch(url).then((response) => {
-//   return response.json();
-// });
-
-// $.ajax({
-//   url:
-//     "https://raw.githubusercontent.com/ConsultaRemedios/frontend-challenge/master/products.json",
-//   type: "GET",
-//   success: function (result) {
-//     let data = JSON.parse(result);
-//
-
-//   },
-// });
