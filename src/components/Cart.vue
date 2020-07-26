0d48ab29781cf81160a49b6b474b2657e3469a8f
Marcelo Seiji <ikuta.marcelo@gmail.com>
2020-07-26 15:16:18 -0300

    card component structure added

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="row" id="cart">
-    Carrinho
+  <div class="row cart" id="cart">
+    <b-card tag="article" class="mb-2">
+      <div class="cart-part head">
+        <h3 class="title">Carrinho</h3>
+      </div>
+      <div class="cart-part body">
+        <b-img :src="require(`@/assets/cart-icon.svg`)" fluid alt="Cart Image"></b-img>
+        <b-card-text class="text">Até o momento seu carrinho está vazio</b-card-text>
+      </div>
+      <div class="cart-part footer">
+        <b-button class="btn" href="#" variant="primary">Finzalizar compra</b-button>
+      </div>
+    </b-card>
   </div>
 </template>
 
@@ -11,10 +22,34 @@ export default {
   data() {
     return {};
   },
-  methods: {},
+  methods: {}
 };
 </script>
 
 <style lang="scss">
 @import "../variables.scss";
+
+.cart {
+  margin-top: 44px;
+  min-height: 325px;
+  .card-body {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+  }
+  .cart-part {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+  .head {
+    justify-content: flex-start;
+  }
+  .body {
+    justify-content: center;
+  }
+  .footer {
+    justify-content: flex-end;
+  }
+}
 </style>
